3169113bf486cae8d48acc3a9ace8bad42363118
tunny17 <tvnji17@gmail.com>
2022-09-13 12:57:10 -0700

    stuff

--- sandbox.js
+++ sandbox.js
@@ -20,17 +20,17 @@ const validate = (e) => {
   e.preventDefault();
   const holderName = document.getElementById('holderName');
   const cardNumber = document.getElementById('cardNumber');
-  if (holderName.value === "") {
-    document.getElementById('nameP').classList.toggle('visibility');
+  if (holderName.value === "" && cardNumber.value === "") {
+    document.getElementById('nameP').innerHTML = "help punk"
+    document.getElementById('cardNumber').innerHTML = "help punk"
     holderName.focus();
     return false;
   }
-  if (cardNumber.value === "") {
-    document.getElementById('nameP').classList.remove('visibility')
-    alert("Please enter your email address.");
-    cardNumber.focus();
-    return false;
-  }
+  // if (cardNumber.value === "") {
+  //   document.getElementById('cardNumber').innerHTML = "help punk"
+  //   cardNumber.focus();
+  //   return false;
+  // }
   
   return true;
 }
